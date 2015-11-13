4bd5b827335b51c976af4395e406ef51c42a810d
Sellaginella <AMDudley1230@gmail.com>
2015-11-13 10:40:10 -0500

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,11 @@
+// SilverQuotes Main File
+// Alex, Andrew, Marcus, David
+
+public class Main
+  {
+  
+  public static void main(String[] args)
+    {
+    System.out.println("Hello World!");
+    }
+  }
