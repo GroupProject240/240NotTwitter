4826bdb32fcfb426fdda5044628e74374e588e5c
Sellaginella <AMDudley1230@gmail.com>
2015-11-29 18:35:52 -0500

    Update Main.java
    So sorry to old main, but this instantiates the UserPasswordAccessor and PostMessageAccessor, as well as reads the User Password "database" file. Only the Create Account and Exit functions do anything of note.

--- Main.java
+++ Main.java
@@ -1,51 +1,75 @@
 // SilverQuotes Main File
 // Alex, Andrew, Marcus, David
 
-import java.io.*;
 import java.util.*;
 
 public class Main
-  {
-  
-  public static void main(String[] args)
     {
-	  	 
-        System.out.println("Welcome to SilverQuotes");
-        System.out.println("Press 1 to create a account");
-        System.out.println("Press 2 to login into your account");
-        System.out.println("Press 3 to Ghost Browse");
-        System.out.println("Press 4 to quit");
+    public static void main(String[] args)
+        {
+        UserPasswordAccessor UPA = new UserPasswordAccessor();
+        PostMessageAccessor PMA = new PostMessageAccessor();
         
+        System.out.println("Welcome to Silver Quotes!");
         Scanner in = new Scanner(System.in);
+        // 'run' necessary to keep while loop running until an error or the Exit command is given
+        boolean run = true;
         
-        int i = in.nextInt();
-        
-        if(i == 1){
-            User Jack = new User();
-            String usName = CreateUser.createUsername();
-            String paWord = CreateUser.createPassword();
-            Jack.setUsername(usName);
-            Jack.setPassword(paWord);
-            String uniqueID = UUID.randomUUID().toString();
-            Jack.setId(uniqueID);
+        while (run == true)
+            {
+            System.out.println("(C)reate an account");
+            System.out.println("(L)ogin to your account");
+            System.out.println("(G)host browse");
+            System.out.println("(E)xit Silver Quotes");
+            String input = in.nextLine();
+    
+            if(input == "C")
+                {
+                String inName;
+                String inPass;
+                System.out.println("Please input desired username:");
+                
+                input = in.nextLine();
+                // need to make sure input for username is alphanumeric, within size requirements,
+                // and is not already in use
+                inName = input;
+                System.out.println("Please input desired password:");
+                input = in.nextLine();
+                // need to make sure password meets requirements
+                inPass = input;
+                
+                // User does not give account ID, is randomly generated
+                // create new user, assign variable values
+                User nUser = new User();
+                nUser.setUserName(inName);
+                nUser.setPassword(inPass);
+                // For now, it will set ID to '1111111'
+                nUser.setID("1111111");
+                UPA.addAccount(nUser);
+                
+                System.out.println("Congratulations! You've created an account!");
+                System.out.println("Here are the details of the newly made account:");
+                System.out.println("Username: "+nUser.getUserName());
+                System.out.println("Password: "+nUser.getPassword());
+                System.out.println("Account No.: "+nUser.getID());
+                }
             
-            System.out.println("You've created an account!");
-            System.out.println("Here are your details below:");
+            if (input == "L")
+                {
+                System.out.println("Despite being an inherent part of a Twitter Clone, this functionality is unavailable at this time~");
+                }
             
-            System.out.println("Username: "+Jack.getUsername());
-            System.out.println("Password: "+Jack.getPassword());
-            System.out.println("Account No.: "+Jack.getId());
-           
+            if (input == "G")
+                {
+                System.out.println("Despite being an inherent part of a Twitter Clone, this functionality is unavailable at this time~");
+                }
             
-        }else if(i == 2){
-        	
-        }else if(i == 3){
-        	
-        }else if(i == 4){
-        	
-        }else{
-        	System.out.println("Invalid selection");
+            if (input == "E")
+                {
+                System.out.println("Thanks for using Silver Quotes, have a wonderful day~");
+                run = false;
+                }
+            }
+        in.close();
         }
-                     
     }
- }
