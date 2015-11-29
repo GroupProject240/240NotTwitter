6b3dde27a4f81f51d5e1a5784f2e415d14d7f392
Sellaginella <AMDudley1230@gmail.com>
2015-11-29 18:29:13 -0500

    Create Post.java
    Very basic, should be expanded upon

---
+++ Post.java
@@ -0,0 +1,12 @@
+// Post Info Class File
+// Alex Dudley
+
+public class Post
+    {
+    // public or private
+    protected String status;
+    // some username
+    protected String poster;
+    // the actual post, replete with any included hashtags
+    protected String message;
+    }
