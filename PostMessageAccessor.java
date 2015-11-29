f4c52153110de0f65d04ba10d673d2b9cfa8aba6
Sellaginella <AMDudley1230@gmail.com>
2015-11-29 18:14:19 -0500

    Create PostMessageAccessor.java
    Has an odd error, doesn't know what an ArrayList is despite importing java.io.* . Will need some looking in to.

---
+++ PostMessageAccessor.java
@@ -0,0 +1,10 @@
+// Post Message "Database" Accessor Class File
+// Alex Dudley
+
+import java.io.*;
+
+public class PostMessageAccessor
+    {
+    protected ArrayList<Post> postInfoList;
+    
+    }
