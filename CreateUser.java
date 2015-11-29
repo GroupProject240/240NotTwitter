26a79eaeb60a9cbc34d5911376138bd05ad0b268
RCF0900 <abeamer@mail.umw.edu>
2015-11-29 11:56:12 -0500

    Update CreateUser.java

--- CreateUser.java
+++ CreateUser.java
@@ -1,10 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package testtwitterdude;
-
 import java.io.*;
 import java.util.*;
 /**
@@ -14,36 +7,24 @@ import java.util.*;
 public class CreateUser {
     
     public static String createUsername() {
-    
-     System.out.println("Alright then motherfucker:");
      
      Scanner user = new Scanner(System.in);
      
      System.out.println("Enter your desired username:");
-     
      String Username = user.next();
      
      System.out.println(Username + " will be your username for the system.");
-     
-     
+          
      return Username;
-}
+    }
     
-     public static String createPassword() {
-    
-     System.out.println("Step two bitch:");
+     public static String createPassword() {     
+    	 Scanner user = new Scanner(System.in);
      
-     Scanner user = new Scanner(System.in);
-     
-     System.out.println("Enter your desired password:");
-     
-     String Password= user.next();
-     
-     System.out.println(Password + " will be your password for the system. DO NOT fucking forget it");
+    	 System.out.println("Enter your desired password:");
+    	 String Password= user.next();
+    	 System.out.println(Password + " will be your password for the system.");
      
      return Password;
-}
-    
-    
-    
+     }    
 }
