99260e63aae1219a226b27775375ec6c9dba79c5
RCF0900 <abeamer@mail.umw.edu>
2015-11-29 11:53:19 -0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,18 +1,23 @@
 // SilverQuotes Main File
 // Alex, Andrew, Marcus, David
 
+import java.io.*;
+import java.util.*;
+
 public class Main
   {
   
   public static void main(String[] args)
     {
-        System.out.println("Welcome to this piece of shit.");
+	  	 
+        System.out.println("Welcome to SilverQuotes");
         System.out.println("Press 1 to create a account");
         System.out.println("Press 2 to login into your account");
-        System.out.println("Press 3 to do something else that wasnt the last thing");
-        System.out.println("Press 4 to fuck off and quit bothering me");
+        System.out.println("Press 3 to Ghost Browse");
+        System.out.println("Press 4 to quit");
         
         Scanner in = new Scanner(System.in);
+        
         int i = in.nextInt();
         
         if(i == 1){
@@ -21,22 +26,26 @@ public class Main
             String paWord = CreateUser.createPassword();
             Jack.setUsername(usName);
             Jack.setPassword(paWord);
-            Jack.setId(6549872544321L);
+            String uniqueID = UUID.randomUUID().toString();
+            Jack.setId(uniqueID);
             
-            System.out.println("Congrats you sad sack of shit! You've created an account!");
+            System.out.println("You've created an account!");
             System.out.println("Here are your details below:");
             
             System.out.println("Username: "+Jack.getUsername());
             System.out.println("Password: "+Jack.getPassword());
             System.out.println("Account No.: "+Jack.getId());
+           
             
-            
-            
+        }else if(i == 2){
+        	
+        }else if(i == 3){
+        	
+        }else if(i == 4){
+        	
+        }else{
+        	System.out.println("Invalid selection");
         }
-        
-        
-        
-        
+                     
     }
  }
- 
