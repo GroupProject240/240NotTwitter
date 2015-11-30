eb8cba1447cc30fd80783cff9844508e87b38935
Sellaginella <AMDudley1230@gmail.com>
2015-11-29 20:06:37 -0500

    Update Post.java
    Gave Post class getters and setters

--- Post.java
+++ Post.java
@@ -9,4 +9,46 @@ public class Post
     protected String poster;
     // the actual post, replete with any included hashtags
     protected String message;
+    
+    Post()
+        {
+        status = "private";
+        poster = "???????";
+        message = "?";
+        }
+    
+    public void setStatusAsPublic()
+        {
+        status = "public";
+        }
+    
+    public void setStatusAsPrivate()
+        {
+        status = "private";
+        }
+    
+    public void setPoster(String inputPoster)
+        {
+        poster = inputPoster;
+        }
+    
+    public void setMess(String inputMess)
+        {
+        message = inputMess;
+        }
+    
+    public String getStatus()
+        {
+        return status;
+        }
+    
+    public String getPoster()
+        {
+        return poster;
+        }
+    
+    public String getMess()
+        {
+        return message;
+        }
     }
